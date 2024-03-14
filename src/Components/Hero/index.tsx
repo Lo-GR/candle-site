e5653a06c3657a48e9274afbb25755bd336faa4a
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2024-03-13 22:39:15 -0700

    correct mistake

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -17,7 +17,7 @@ const Hero = ({ h1, image, cta }: HeroProps) => {
     // <div style={{ backgroundImage: "url(./assets/images/GaelicSmokeHero.jpg)" } as React.CSSProperties} className="relative h-[400px] overflow-hidden">
     <BackgroundImage src={image} className="bg-[rgba(139,69,19,.4)] bg-blend-overlay relative overflow-hidden h-[200px] lg:h-[400px]">
       <MainContainer classN=' flex flex-cool justify-center items-center relative h-full lg:justify-start'>
-        <div className='bg-[#17331F] p-14 outline outline-2 outline-black outline-offset-8 shadow-lg shadow-inner'>
+        <div className='bg-[#17331F] p-14 outline outline-2 outline-black outline-offset-8 shadow-inner'>
           <h1 className='my-auto '>
             {h1}
           </h1>
