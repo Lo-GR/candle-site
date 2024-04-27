af5fe1e4d359d4d78639df681e8f5edf05ef7762
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2024-04-27 16:20:27 -0700

    minor work

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -13,7 +13,7 @@ type HeroProps = {
 const Hero = ({ h1, image, cta }: HeroProps) => {
 
   return (
-    <BackgroundImage src={image} className="bg-[rgba(139,69,19,.4)] bg-blend-overlay relative overflow-hidden h-[200px] lg:h-[400px]">
+    <BackgroundImage src={image} className="bg-[rgba(139,69,19,.4)] bg-blend-overlay relative overflow-hidden h-[400px] lg:h-[800px]">
       <MainContainer classN=' flex flex-cool justify-center items-center relative h-full lg:justify-start'>
         <div className='bg-green-moss-400 transition-all text-center p-6 outline outline-2 outline-black outline-offset-8 shadow-inner lg:p-14 '>
           <h1 className='my-auto'>
